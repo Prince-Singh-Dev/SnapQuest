84e53521869a6f8ba2b8b3b13a7ad247fc89a2d7
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-08-04 19:28:55 +0530

    Added Badges and Perks For User to complete its Lessons

--- backend/controllers/lessonProgressController.js
+++ backend/controllers/lessonProgressController.js
@@ -1,5 +1,6 @@
 const LessonProgress = require('../models/LessonProgress');
 const Lesson = require('../models/Lesson');
+const User = require('../models/User');
 
 // @desc Submit a lesson
 // @route POST/api/lessons/:lessonId/submit
@@ -29,7 +30,42 @@ const submitLesson = async(req,res)=>{
             isCompleted:true,
         });
 
-        res.status(201).json(submission);
+        // Updating User Stats
+        const user = await User.findById(userId);
+        user.points += 10;
+
+        // Updating Perks every 50 Points
+        if(user.points >= 50){
+            user.perks = Math.floor(user.points/50);
+        }
+
+        //Checking total lesson completed
+        const completeCount = await LessonProgress.countDocuments({user : userId, isCompleted:true});
+
+        //Defining Badge Milestones
+        const badgeMilestones = [
+            {count:10,name:'Rookie'},
+            {count:50,name:'Skilled'},
+            {count:100,name:'Pro'},
+            {count:200,name:'Master'},
+        ]
+
+        // Adding Badge if Milestone is reached
+        badgeMilestones.forEach(badge =>{
+            if(completedCount >= badge.count && !user.badges.includes(badge.name)){
+                user.badges.push(badge.name);
+            }
+        });
+
+        await user.save();
+
+        res.status(201).json({
+            message : 'Lesson Submited Successfully and rewards updated.',
+            submission,
+            points : user.points,
+            perks : user.perks,
+            badges : user.badges
+        });
     } catch(err){
         console.error('Lesson Submission Error :',err.message);
         res.status(500).json({message:'Server error'});
