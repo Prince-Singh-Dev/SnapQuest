12b3048b3ad94507bfee9a80355cafeff5a3e8fb
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-08-17 12:30:39 +0530

    Added Navbar and Home frontend design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,45 @@
+import {Link} from "react-router-dom";
+import "./Home.css";
+
 function Home(){
-    return <h2>Welcome to SnapQuest Home</h2>;
+    return(
+        <div className="home-container">
+            {/* Hero Section */}
+            <section className="hero">
+                <h1>Welcome to SnapQuest</h1>
+                <p>Learn photography step by step , unlock challenges and climb the global leaderboard!</p>
+                <Link to="/signup">
+                    <button className="cta-btn">Get Started →</button>
+                </Link>
+            </section>
+
+            {/* Features Section */}
+            <section className="features">
+                <div className="feature-card">
+                    <h2>✍️ Lessons </h2>
+                    <p>Complete Lessons to earn points , badges and unlock challenges.</p>
+                    <Link to="/lessons">Explore Lessons →</Link>
+                </div>
+
+                <div className="feature-card">
+                    <h2>🏆 Challenges</h2>
+                    <p>Compete in photography challenges and rise in the rankings.</p>
+                    <Link to="/challenges">Join Challenges →</Link>
+                </div>
+
+                <div className="feature-card">
+                    <h2>👤 Profile</h2>
+                    <p>Track your progress , Badges and world rankings.</p>
+                    <Link to="/profile">View Profile →</Link>
+                </div>
+            </section>
+
+            {/*Footer*/}
+            <footer className ="footer">
+                <p>© {new Date().getFullYear()} SnapQuest. All rights reserved.</p>
+            </footer>
+        </div>
+    );
 }
+
 export default Home;
