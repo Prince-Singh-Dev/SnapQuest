5c507490c8902c91046843d2f45a2fc68b96ccaf
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-09-07 12:55:42 +0530

    Added The Protected Routes and AuthProtection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router , Routes , Route , Link } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 import Lessons from "./pages/Lessons";
 import Challenges from "./pages/Challenges";
@@ -12,35 +13,37 @@ import "./App.css";
 
 function App(){
   return (
-    <Router>
-      <div>
-        {/* Navbar */}
-        <nav className="navbar">
-          <h1 className="logo">SnapQuest</h1>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/lessons">Lessons</Link></li>
-            <li><Link to="/challenges">Challenges</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-          </ul>
-        </nav>
+    <AuthProvider>
+      <Router>
+        <div>
+          {/* Navbar */}
+          <nav className="navbar">
+            <h1 className="logo">SnapQuest</h1>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/lessons">Lessons</Link></li>
+              <li><Link to="/challenges">Challenges</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+            </ul>
+          </nav>
 
-        {/* Page Routes */}
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/lessons" element={<Lessons/>}/>
-          <Route path="/challenges" element={<Challenges />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/challenges" element={<Challenges/>} />
-          <Route path="/challenges/:id" element={<ChallengeDetails/>}/>
-          <Route path="/voting" element={<Voting/>}/>
-        </Routes>
-      </div>
-    </Router>
+          {/* Page Routes */}
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/lessons" element={<Lessons/>}/>
+            <Route path="/challenges" element={<Challenges />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/challenges" element={<Challenges/>} />
+            <Route path="/challenges/:id" element={<ChallengeDetails/>}/>
+            <Route path="/voting" element={<Voting/>}/>
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
