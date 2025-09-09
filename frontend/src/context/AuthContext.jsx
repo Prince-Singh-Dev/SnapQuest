39dac09252b5be6e3b3f53715abc214b1f1b83e4
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-09-09 15:32:54 +0530

    Updated User Schema

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,43 +1,42 @@
-import { Children } from "react";
-import { createContext , useState , useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({Children}) => {
-    const [user,setUser] = useState(null);
-    const [token , setToken] = useState(localStorage.getItem("token") || null);
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
 
-    useEffect(() => {
-        if (token){
-            fetch("http://localhost:5000/api/auth/me",{
-                headers : {Authorization : `Bearer ${token}`},
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                if(!data.error) setUser(data);
-                else{
-                    setToken(null);
-                    localStorage.removeItem("token");
-                }
-            });
-        }
-    },[token]);
+  useEffect(() => {
+    if (token) {
+      fetch("http://localhost:5000/api/auth/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data.error) setUser(data);
+          else {
+            setToken(null);
+            localStorage.removeItem("token");
+          }
+        });
+    }
+  }, [token]);
 
-    const login = (token , userData) => {
-        setToken(token);
-        setUser(userData);
-        localStorage.setItem("token",token);
-    };
+  const login = (token, userData) => {
+    setToken(token);
+    setUser(userData);
+    localStorage.setItem("token", token);
+  };
 
-    const logout = () => {
-        setToken(null);
-        setUser(null);
-        localStorage.removeItem("token");
-    };
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem("token");
+  };
 
-    return(
-        <AuthContext.Provider value={{user,token,login,logout,setUser}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
